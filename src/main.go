076df0e0b1067905a58a50322d82211d36d36f1d
akramsaouri <akram@hiddenfounders.com>
2019-01-11 19:10:45 +0100

    test github action

--- src/main.go
+++ src/main.go
@@ -29,6 +29,9 @@ func main() {
 	}
 
 	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello")
+	})
 	r.HandleFunc("/videos", listVideos).Methods("GET")
 	r.HandleFunc("/videos/{title}", viewVideo).Methods("GET")
 	r.HandleFunc("/pubsubhubbub/subscribe", subscribe).Methods("POST")
