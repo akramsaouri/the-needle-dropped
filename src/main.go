936e439d8d69f5a2b21924f0edddffc58b33cf19
akramsaouri <akram@hiddenfounders.com>
2019-01-12 12:49:22 +0100

    remove / path

--- src/main.go
+++ src/main.go
@@ -29,9 +29,6 @@ func main() {
 	}
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello*")
-	})
 	r.HandleFunc("/videos", listVideos).Methods("GET")
 	r.HandleFunc("/videos/{title}", viewVideo).Methods("GET")
 	r.HandleFunc("/pubsubhubbub/subscribe", subscribe).Methods("POST")
